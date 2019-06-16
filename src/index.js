a0cf805fcdb65aee27a1ab5ab27d6190b0ecb916
Kidfalsh <www.theflashdye@163.com>
2019-06-16 10:52:00 +0800

    fisrt react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
+// PWA progressive web application
+// https 协议服务器上
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// 可以注释掉 保证在断网情况下会正常显示
+// serviceWorker.unregister();
